b98ca712a5211622ac494db78cef0d8d49b8f677
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-06 14:25:50 -0500

    working on profile page

--- src/components/Profile/ProfileComponent.js
+++ src/components/Profile/ProfileComponent.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import "./profile.css";
 
 function ProfileComponent() {
     return (
         <div>
-            Profile page
+            <div id="profilePicCard" className="card">
+                <div className ="card-body">
+                    <img id="profilePic" src="https://smhlancers.org/wp-content/uploads/2016/06/profile-placeholder.png" alt="profilepic"></img>
+                </div>
+            </div>
         </div>
     )
 }

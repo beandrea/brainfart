75e3c040234386aeb55aa8e01d401f5dd318e68b
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-08 15:09:43 -0500

    created card for users info

--- src/components/Profile/ProfileComponent.js
+++ src/components/Profile/ProfileComponent.js
@@ -5,8 +5,36 @@ function ProfileComponent() {
     return (
         <div>
             <div id="profilePicCard" className="card">
-                <div className ="card-body">
+                <div className="card-body">
+                    <div id="usernameCard" className="card-body">
+                        <div id="usernameDisplay">Username</div>
+                    </div>
                     <img id="profilePic" src="https://smhlancers.org/wp-content/uploads/2016/06/profile-placeholder.png" alt="profilepic"></img>
+                </div>
+            </div>
+            <div id="userInfoDiv" className="card">
+                <div className="card-body">
+                    <h5>My Stats</h5>
+                </div>
+                <div className="card infoCards">
+                    <div className="card-body">
+                        highest grade
+                    </div>
+                </div>
+                <div className="card infoCards">
+                    <div className="card-body">
+                        lowest grade
+                    </div>
+                </div>
+                <div className="card infoCards">
+                    <div className="card-body">
+                        list of created
+                    </div>
+                </div>
+                <div className="card infoCards">
+                    <div className="card-body">
+                        list of previous quizes
+                    </div>
                 </div>
             </div>
         </div>

fdcb0dabf1f95a95970f4017cac7a82d982c4908
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-08 19:46:14 -0500

    added more styling

--- src/components/Profile/ProfileComponent.js
+++ src/components/Profile/ProfileComponent.js
@@ -7,7 +7,7 @@ function ProfileComponent() {
             <div id="profilePicCard" className="card">
                 <div className="card-body">
                     <div id="usernameCard" className="card-body">
-                        <div id="usernameDisplay">Username</div>
+                        <div id="usernameDisplay">Username Displayed Herei</div>
                     </div>
                     <img id="profilePic" src="https://smhlancers.org/wp-content/uploads/2016/06/profile-placeholder.png" alt="profilepic"></img>
                 </div>
@@ -19,21 +19,21 @@ function ProfileComponent() {
                 <div className="card infoCards">
                     <h5>Highest Grade</h5>
                     <div className="card-body quizGrades background">
-                        <h6 className="quizName">Quiz Name:{ }</h6>
-                        <h6 id="grade">Grade:{ }</h6>
+                        <h6 className="quizName">Quiz Name: { }</h6>
+                        <h6 id="grade">Grade: { }</h6>
                     </div>
                 </div>
                 <div className="card infoCards">
                     <h5>Lowest Grade</h5>
                     <div className="card-body quizGrades background">
-                        <h6 className="quizName">Quiz Name:{ }</h6>
-                        <h6 id="grade">Grade:{ }</h6>
+                        <h6 className="quizName">Quiz Name: { }</h6>
+                        <h6 id="grade">Grade: { }</h6>
                     </div>
                 </div>
                 <div className="card infoCards">
                     <h5> My Created Quizes</h5>
                     <div id="created" className="card-body background">
-                        <h6 className="quizName">Quiz Name:{ }</h6>
+                        <h6 className="quizName">Quiz Name: { }</h6>
                         <div id="btns">
                             <button className="btn btn-outline-success createdBtns">Add</button>
                             <button className="btn btn-outline-warning createdBtns">Edit</button>
@@ -43,7 +43,7 @@ function ProfileComponent() {
                 <div className="card infoCards">
                     <h5>My Previous Quizes</h5>
                     <div className="card-body background">
-                        <h6 className="quizName">Quiz Name:{ }</h6>
+                        <h6 className="quizName">Quiz Name: { }</h6>
                     </div>
                 </div>
             </div>

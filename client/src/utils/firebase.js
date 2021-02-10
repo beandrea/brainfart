3b5470f69e06bc725c67f04cae72e1339a124e3a
David Bleiweiss <dbleiweiss123@gmail.com>
2021-02-09 21:11:30 -0500

    throw out everything except changes in routes

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -10,53 +10,69 @@ const firebaseConfig = {
     databaseURL: "https://brainfart-83cc4.firebaseio.com",
     projectId: "brainfart-83cc4",
     storageBucket: "brainfart-83cc4.appspot.com",
-    messagingSenderId: "106322462222478724836",
-    appId: "1:530808334788:web:f5504cf15e5e6e88e95c91"
+    messagingSenderId: "996619792238",
+    appId: "1:996619792238:web:cd2a898554294ca2aa1b7f"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+export const signupWithEmail = (email,password) => {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in 
+        var user = userCredential.user;
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ..
+      });
+}
 
-const provider = new firebase.auth.GoogleAuthProvider();
-export const signInWithGoogle = () => {
-    auth.signInWithPopup(provider);
-};
 
-export const generateUserDocument = async (user, additionalData) => {
-    if (!user) return;
+// export const auth = firebase.auth();
+// export const firestore = firebase.firestore();
 
-    const userRef = firestore.doc(`users/${user.uid}`);
-    const snapshot = await userRef.get();
+// const provider = new firebase.auth.GoogleAuthProvider();
+// export const signInWithGoogle = () => {
+//     auth.signInWithPopup(provider);
+// };
 
-    if (!snapshot.exists) {
-        const { email, displayName, photoURL } = user;
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                photoURL,
-                ...additionalData
-            });
-        } catch (error) {
-            console.error("Error creating user document", error);
-        }
-    }
-    return getUserDocument(user.uid);
-};
+// export const generateUserDocument = async (user, additionalData) => {
+//     if (!user) return;
 
-const getUserDocument = async uid => {
-    if (!uid) return null;
-    try {
-        const userDocument = await firestore.doc(`users/${uid}`).get();
+//     const userRef = firestore.doc(`users/${user.uid}`);
+//     const snapshot = await userRef.get();
 
-        return {
-            uid,
-            ...userDocument.data()
-        };
-    } catch (error) {
-        console.error("Error fetching user", error);
-    }
-};
+//     if (!snapshot.exists) {
+//         const { email, displayName, photoURL } = user;
+//         try {
+//             await userRef.set({
+//                 displayName,
+//                 email,
+//                 photoURL,
+//                 ...additionalData
+//             });
+//         } catch (error) {
+//             console.error("Error creating user document", error);
+//         }
+//     }
+//     return getUserDocument(user.uid);
+// };
+
+// const getUserDocument = async uid => {
+//     if (!uid) return null;
+//     try {
+//         const userDocument = await firestore.doc(`users/${uid}`).get();
+
+//         return {
+//             uid,
+//             ...userDocument.data()
+//         };
+//     } catch (error) {
+//         console.error("Error fetching user", error);
+//     }
+// };

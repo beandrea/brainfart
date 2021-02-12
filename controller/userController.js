9cbf5524183831a19877a485b3e252482667596c
David Bleiweiss <dbleiweiss123@gmail.com>
2021-02-11 21:44:11 -0500

    testing route to save user data

--- controller/userController.js
+++ controller/userController.js
@@ -4,9 +4,8 @@ module.exports = {
     createUser: function (req, res) {
         //user firebase method to create user
         
-
         // if succesful us route to create method
-        db.User.create(req.body)
+        db.create(req.body)
             .then(dbModel => res.JSON(dbModel))
             .catch(err => console.log(err));
     },

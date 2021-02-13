ce5d16fcaa0a64705ec97d2d280627087d227397
Andrea <arkflame99@gmail.com>
2021-02-13 11:12:18 -0500

    compSci cat

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./Card.css"
-import {Link} from 'react-router-dom';
 
 function Card(props) {
     return (
@@ -8,7 +7,6 @@ function Card(props) {
             <img src={props.image} className="card-img-top" alt={props.title}></img>
             <div className="card-body">
                 <h5 className="card-title">{props.title}</h5>
-                {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
             </div>
         </div>
     )

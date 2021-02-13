07bc4f583412662b733a420a7eca7f15b6262ab4
Andrea <arkflame99@gmail.com>
2021-02-13 11:32:29 -0500

    frontend

--- client/src/components/ComputerScience/CompSci.js
+++ client/src/components/ComputerScience/CompSci.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./compSci.css";
+import API from "../../utils/API";
 
 function CompSci() {
+    const [cs, setCs] = useState([]);
+    
+    function getQuizes() {
+        API.getQuiz(18).then(res => {setCs(res.json)});
+        console.log(cs);
+    }
+
     return (
         <div>
             Hi Bob!

dd38b71aebe481ee9a2e511ff5433797917d931d
Anthony Parrino <anthony21parrino21@gmail.com>
2021-02-12 21:42:39 -0500

    fixed mongo bug

--- client/src/pages/Signin.js
+++ client/src/pages/Signin.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { signInWithGoogle, auth } from "../utils/firebase";
+import { auth } from "../utils/firebase";
 import "./style/login.css"
 
 const SignIn = () => {

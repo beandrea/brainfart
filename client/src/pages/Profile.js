f3627258493fa24b9423e02bb4f255f39029123b
David Bleiweiss <dbleiweiss123@gmail.com>
2021-02-13 11:16:09 -0500

    redirect after signin

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ProfileComponent from "../components/Profile/ProfileComponent";
+import firebase from "firebase/app";
+import Redirect from "react-dom";
 
 const ProfilePage = () => {
+    if (firebase.auth().currentUser === null) {
+        return <Redirect to="/signin" />;
+    }
 
     return (
         <div className="mx-auto w-11/12 md:w-2/4 py-8 px-4 md:px-8">
